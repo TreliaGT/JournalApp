82311630d037871bc723637bb6883d67a08dff9f
GG <georgia@stormbox.com.au>
2024-03-20 12:26:25 +0800

    theming the document

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 from journal import Journal
 from tasks import TasksTab
+from tkinter import ttk  # Normal Tkinter.* widgets are not themed!
+from ttkthemes import ThemedTk
 
 
 def main():
-    root = tk.Tk()
+    root = ThemedTk(theme="arc")
     app = Journal(root)
     tasks_tab = TasksTab(app.notebook)  # Add the tasks tab
+ 
     root.mainloop()
 
 if __name__ == "__main__":
